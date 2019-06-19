1091edf1367dfc633d3a0f14cea53e228452dd93
Tony Baker <this.tonybaker@gmail.com>
2019-06-19 00:48:44 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-
+require 'pry'
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
@@ -10,4 +10,35 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  post '/articles' do
+    article = Article.new(title: params[:title], content: params[:content])
+    article.save
+
+    redirect to "/articles/#{article.id}"
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id].to_i)
+    erb :show
+  end
+
+  get '/articles' do
+    @articles = Article.all
+
+    erb :index
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id].to_i)
+    erb :edit
+  end
+
+
+
+
 end
